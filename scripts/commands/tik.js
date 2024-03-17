02bb6e5cb248a8f8a18a19ff6eae6514555f80db
RABBY-404 <163514659+RABBY-404@users.noreply.github.com>
2024-03-17 13:50:07 +0600

    Update tik.js

--- scripts/commands/tik.js
+++ scripts/commands/tik.js
@@ -6,7 +6,7 @@ module.exports = {
     version: "2.0.0",
     permission: 0,
     credits: "Nayan",
-    description: "Download video from facebook",
+    description: "Download video from Facebook",
     prefix: true,
     category: "admin",
     usages: "link",
@@ -17,7 +17,6 @@ module.exports = {
   },
 
   start: async function ({ nayan, events, args }) {
-
     nayan.setMessageReaction("😘", events.messageID, (err) => {}, true);
     nayan.sendTypingIndicator(events.threadID, true);
 
@@ -27,16 +26,15 @@ module.exports = {
     const fs = require("fs");
     const axios = require("axios");
     const request = require("request");
+    const res = await axios.get(`https://raw.githubusercontent.com/MR-NAYAN-404/ERROR/main/error.json`);
+    const error = res.data.error;
+
+    if (!args[0]) return nayan.reply("[ ! ] Input link.", threadID, messageID);
+
+    const content = args.join(" ");
+    if (!args[1]) nayan.reply(`𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝐕𝐈𝐃𝐄𝐎 𝐅𝐎𝐑 𝐘𝐎𝐔\n\n𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝟖...`, events.threadID, (err, info) => setTimeout(() => { nayan.unsendMessage(info.messageID) }, 20000));
 
     try {
-      const res = await axios.get(`https://raw.githubusercontent.com/MR-NAYAN-404/ERROR/main/error.json`);
-      const error = res.data.error;
-      const prompt = args.join(" ");
-      if (!args[0]) return nayan.sendMessage("[ ! ] Input link.", threadID, messageID);
-
-      const content = args.join(" ");
-      if (!args[1]) nayan.reply(`𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐈𝐍𝐆 𝐕𝐈𝐃𝐄𝐎 𝐅𝐎𝐑 𝐘𝐎𝐔\n\n𝐏𝐋𝐄𝐀𝐒𝐄 𝐖𝟖...`, events.threadID, (err, info) => setTimeout(() => { nayan.unsendMessage(info.messageID) }, 20000));
-
       const res = await tikdown(content);
       console.log(res);
       const file = fs.createWriteStream(__dirname + '/cache/tik.mp4');
@@ -55,7 +53,8 @@ module.exports = {
         }, 5000);
       });
     } catch (err) {
-      nayan.reply(`${error}`, events.threadID, events.messageID);
+      console.error(err); // Log the error for debugging
+      nayan.reply(`Failed to download the TikTok video: ${error}`, events.threadID, events.messageID);
     }
   }
 };
