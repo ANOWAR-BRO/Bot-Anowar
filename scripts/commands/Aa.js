11b2b7c14d6fbd4d32fc53f3429553abd576b7ba
RABBY-404 <163514659+RABBY-404@users.noreply.github.com>
2024-03-19 10:50:16 +0600

    Update Aa.js

--- scripts/commands/Aa.js
+++ scripts/commands/Aa.js
@@ -1,43 +1,40 @@
 const puppeteer = require('puppeteer');
 
 module.exports.config = {
-    name: "call",
+    name: "gemini",
     version: "1.0.0",
-    permission: 2,
+    permission: 0,
     credits: "Rahad",
-    description: "Initiate a call to the specified number",
+    description: "this command will help you to answer your questions!",
     prefix: true,
-    category: "call",
-    usages: "call <phone_number>",
-    cooldowns: 5,
+    category: "Artificial Intelligence",
+    usages: "(name_of_cmd) [query]",
+    cooldowns: 0,
     dependencies: {}
-};
-
-module.exports.run = async function({ api, event, args }) {
-    const { threadID } = event;
-    const phoneNumber = args[0];
-
-    if (!phoneNumber) {
-        api.sendMessage("Please provide a phone number in the format: call <phone_number>", threadID);
-        return;
-    }
-
+};  
+  
+module.exports.run = async ({ api, event, args }) => {
+    const axios = require("axios");
+    let prompt = args.join(" "),
+      uid = event.senderID,
+      url;
+    if (!prompt) return api.sendMessage(`Please enter a prompt.`, event.threadID, event.messageID);
+    
     try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(`https://our-api.000webhostapp.com/api/call.php?number=${encodeURIComponent(phoneNumber)}`);
-        
-       
-        await page.waitFor(5000);
-        
-        const content = await page.content();
-        console.log("Page Content:", content);
-        
-        await browser.close();
-        
-        api.sendMessage("Call request initiated successfully!", threadID);
-    } catch (error) {
-        console.error(error);
-        api.sendMessage("Call request initiated successfully!", threadID);
-    }
-};
+      const apis = `https://gemini-api.replit.app`;
+      if (event.type == "message_reply"){
+        if (event.messageReply.attachments[0]?.type == "photo"){
+        url = encodeURIComponent(event.messageReply.attachments[0].url);
+        const res = (await axios.get(apis + "/gemini?prompt="+prompt+"&url="+url+"&uid="+uid)).data;
+        return api.sendMessage(`${res.gemini}`, event.threadID, event.messageID)
+        } else {
+          return api.sendMessage('Please reply to an image.', event.threadID, event.messageID)
+        }
+      }
+      const rest = (await axios.get(apis + "/gemini?prompt=" + prompt + "&uid=" + uid)).data;
+      return api.sendMessage(rest.gemini, event.threadID, event.messageID)
+    } catch (e) {
+      console.log(e);
+      return api.sendMessage(e.message, event.threadID, event.messageID);
+    } //end of catch
+  };
