e16882561decc4b97366f6103e8c4a609ff3ad45
ANOWAR-BRO <137695962+ANOWAR-BRO@users.noreply.github.com>
2024-04-07 11:54:03 +0600

    Update Attitude.js

--- scripts/commands/Attitude.js
+++ scripts/commands/Attitude.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-  name: "bio",
+  name: "Bio",
   version: "0.0.2",
   permission: 0,
   prefix: false,
@@ -17,8 +17,9 @@ module.exports.run = async function({ api, event, args, Users }) {
     const prompt = args.join(" ");
     var id = event.senderID;
     var name = await Users.getNameUser(event.senderID);
-    var tl = "hmm"];
+    var tl = ["হুম জানু" ,];
     var rand = tl[Math.floor(Math.random() * tl.length)];
+    if (!prompt) return api.sendMessage(`⎯͢⎯⃝😽🪽🪄𝗦𝘆𝗸𝗼 ❥᭄🐲𝗔𝗻𝗼𝘄𝗮𝗿 ❥᭄ ⑅⃝🌺🐰\n\n╰➤ 👤 𝐃𝐞𝐚𝐫 『${name}』,\n\n╰➤ 🗣️ ${rand}\n\n⎯͢⎯⃝😽🪽🪄𝗦𝘆𝗸𝗼 ❥᭄🐲𝗔𝗻𝗼𝘄𝗮𝗿 ❥᭄ ⑅⃝🌺🐰`, event.threadID, event.messageID);
     const res = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=bn&message=${prompt}&filter=true`);
     console.log(res.data);
     const respond = res.data.success;
